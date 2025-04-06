58e582616fd2d6bb26e9dc3f008ffd471b9124f7
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-06 23:38:22 +0900

    Put all in span

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -5,7 +5,7 @@ export default new Catalog<Vocabulary>("en", {
   "main/stop-slot": msg("Push!"),
   "mode/advanced": msg("advanced mode"),
   "mode/basic": msg("basic mode"),
-  "result/description": msg("<span>Your personality may be</span>: <result/>"),
+  "result/description": msg("<span>Your personality may be: </span><result/>"),
   "result/go-back": msg("Go back"),
   "result/title": msg("Result"),
   "title/start": msg("Start"),
